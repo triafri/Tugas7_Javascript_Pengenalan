295922749a83489b62eefe4dbbde9a44298c4876
TRI AFRILIYANTI <71365606+triafri@users.noreply.github.com>
2020-09-18 14:38:38 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+//for biasa//
+ar tBadan = [100, 110, 120, 130, 140, 150, 160, 170, 180,190]
+for(i=0 ; i<tBadan.length; i++){
+	console.log(tBadan[i])
+}
+
+//For - of//
+var tBadan = [100, 110, 120, 130, 140, 150, 160, 170, 180,190]
+for(let tb of tBadan){
+	console.log(tb)
+}
